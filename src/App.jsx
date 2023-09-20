b830d5294a32f75cea07a6015d555bf692f97269
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-09-20 03:23:40 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,27 +13,32 @@ function Tutors(props) {
   )
 }
 function Schedule({shift}) {
+  const currDate = new Date();
   return (
-    <table>
-      <thead>
-        <tr>
-          {
-            days.map(e => {
+    <div>
+      <h1>CMP Tutor Schedule<br/>{currDate.getMonth()>=6?"Fall":"Spring"} {currDate.getFullYear()}</h1>
+      <table>
+        <thead>
+          <tr>
+            {
+              days.map(e => {
+                return (
+                  <th key = {e}>{e}</th>
+                )
+              })
+            }
+          </tr>
+          <tr>
+            {days.map((e, i) => {
               return (
-                <th key = {e}>{e}</th>
+                <Tutors key={e} info={shift[i]}/>
               )
-            })
-          }
-        </tr>
-        <tr>
-          {days.map((e, i) => {
-            return (
-              <Tutors key={e} info={shift[i]}/>
-            )
-          })}
-        </tr>
-      </thead>      
-    </table>          
+            })}
+          </tr>
+        </thead>      
+      </table>  
+    </div>
+            
   )
 }
 
@@ -47,26 +52,36 @@ export default function App() {
           tutorInfo
         }
       }
-    }`  
-    fetch(`https://graphql.contentful.com/content/v1/spaces/${spaceId}/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ query }),
-    })
-    .then(res => res.json())
-    .then(res => {
-       const tutorInfo = res.data.tutorsCollection.items[0].tutorInfo;
-       const shift = Array(4).fill(""); 
-       for(let student in tutorInfo) {
-        const day = tutorInfo[student].day;
-        const index = days.indexOf(day);
-        shift[index] += tutorInfo[student].name + '\n' + tutorInfo[student].time + '\n';
-       }
-       setInfo(shift); 
-    });    
+    }`
+    if(!info) {
+      fetch(`https://graphql.contentful.com/content/v1/spaces/${spaceId}/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then(res => res.json())
+      .then(res => {
+         const tutorInfo = res.data.tutorsCollection.items[0].tutorInfo;
+         const shift = Array(4).fill(""); 
+         for(let student in tutorInfo) {
+           const currInfo = tutorInfo[student];         
+           for(let i = 0; i < currInfo.day.length; i++) {
+            const day = currInfo.day[i];
+            const index = days.indexOf(day);
+            shift[index] += '\n'+ `${currInfo.name} (${currInfo.subject})` + '\n' + currInfo.time[i] + '\n';
+           }        
+         }
+        shift.forEach((e, i, arr) => {
+          arr[i] += '\n';
+        })
+         setInfo(shift); 
+      });
+    } else {
+      console.log('Info ready'); //Development only. Delete for build.
+    }    
   }, []);
   
   if (!info) {
