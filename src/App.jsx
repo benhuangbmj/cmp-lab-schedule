44d61a26f1ab2f33c27d45c80cfd3863ad2407cb
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-10-02 04:26:29 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ const spaceId = import.meta.env.VITE_SPACE_ID;
 const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 const cmaToken = import.meta.env.VITE_CMA_TOKEN;
 
-function Tutors({info}) {
+function Tutors({ info }) {
   return (
     <td>
-      {info[0].map((e, i) => 
-        <div className="left-align" key={e}>          
+      {info[0].map((e, i) =>
+        <div className="left-align" key={e}>
           <img className='profile-pic-small' src={info[1][i]} />
-          <pre>{e}</pre>                   
+          <pre>{e}</pre>
         </div>
-      )}      
+      )}
     </td>
   )
 }
@@ -54,7 +54,7 @@ function Schedule({ shift, courses }) {
 function Personnel({ courses }) {
   return (
     <div>
-      <h2 style={{'textAlign': 'left', 'paddingLeft': '1rem'}}>Who can help you?</h2>
+      <h2 style={{ 'textAlign': 'left', 'paddingLeft': '1rem' }}>Who can help you?</h2>
       <ul>{courses.map(e => (
         <li key={e}><pre>{e}</pre></li>
       ))}
@@ -66,9 +66,131 @@ function Personnel({ courses }) {
 }
 
 function Management({ info, fetchInfo }) {
+  const [newPic, setNewPic] = useState();
+  const [uploadStatus, setUploadStatus] = useState('Upload');
+  useEffect(() => {
+    if (newPic) {
+      const newPicURL = URL.createObjectURL(newPic);
+      setProfile(newPicURL);
+    }
+  }, [newPic]);
+  function ChangeProfile() {
+    function handleChangeProfile(e) {
+      setNewPic(e.target.files[0]);
+    }
+    function uploadPic() {
+      setUploadStatus('Uploading ...');
+      const deleteAsset = async function(assetId) {
+        let currAsset = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/${assetId}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${cmaToken}`
+          }
+        });
+        currAsset = await currAsset.json();
+        const currVersion = currAsset.sys.version;
+        await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/${assetId}/published`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${cmaToken}`,
+            "X-Contentful-Version": currVersion,
+          }
+        });
+        let deletedAsset = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/${assetId}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${cmaToken}`,
+            "X-Contentful-Version": currVersion,
+          }
+        });
+      }
+      const createAsset = async function(file, title) {
+        let uploaded = await fetch(`https://upload.contentful.com/spaces/${spaceId}/uploads`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/octet-stream",
+            Authorization: `Bearer ${cmaToken}`
+          },
+          body: file,
+        });
+        uploaded = await uploaded.json();
+        const uploadedId = uploaded.sys.id;
+        const resources = {
+          "fields": {
+            "title": {
+              "en-US": title
+            },
+            "file": {
+              "en-US": {
+                "contentType": file.type,
+                "fileName": file.name,
+                "uploadFrom": {
+                  "sys": {
+                    "type": "Link",
+                    "linkType": "Upload",
+                    "id": uploadedId,
+                  }
+                }
+              }
+            }
+          }
+        }
+        let created = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/octet-stream",
+            Authorization: `Bearer ${cmaToken}`
+          },
+          body: JSON.stringify(resources),
+        });
+        created = await created.json();
+        const createdId = created.sys.id;
+        const processed = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/${createdId}/files/en-US/process`, {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${cmaToken}`,
+            "X-Contentful-Version": 1
+          }
+        });
+        if (processed.ok) {          
+          setTimeout(async () => {
+            var published = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/${createdId}/published`, {
+              method: 'PUT',
+              headers: {
+                Authorization: `Bearer ${cmaToken}`,
+                "X-Contentful-Version": 2,
+              }
+            });
+            if (published.ok) {
+              alert("Change profile picture sucessfully!");              
+            } else {
+              alert("Change profile picture failed");
+            }
+            setUploadStatus('Upload');
+          }, 1000);
+        }
+      }
+      const tutor = info[selected];
+      if (tutor.profilePic) {
+        //deleteAsset(tutor.profilePic.id);
+        createAsset(newPic, selected);
+      } else {
+        console.log('No');
+      }
+    }
+    return (
+      <span>
+        <label>Change Profile Picture </label>
+        <input type='file' accept='image/*' onChange={handleChangeProfile} />
+        <button className="file-input-button" onClick={uploadPic}>{uploadStatus}</button>
+      </span>
+    )
+  }
+
   const courseOptions = ['MATH102', 'MATH107', 'MATH108', 'MATH111', 'MATH112', 'MATH190', 'MATH198', 'MATH211', 'MATH261', 'MATH270', 'MATH308', 'STAT269', 'STAT292', 'STAT281'];
-  const [selected, setSelected] = useState(false);
   const options = [];
+  const skip = ['courses', 'profilePic'];
+
   Object.entries(info).forEach(e => {
     options.push({ value: e[0], label: e[1].name });
   });
@@ -78,8 +200,11 @@ function Management({ info, fetchInfo }) {
 
   const infoKeys = Array.from(Object.keys(Object.values(info)[0]));
   infoKeys.unshift('username');
+
   const { register, reset, handleSubmit } = useForm();
-  const [tutor, setTutor] = useState({});
+
+  const [profile, setProfile] = useState();
+  const [selected, setSelected] = useState();
 
   const onSubmit = (data) => {
     for (let key in data) {
@@ -89,7 +214,7 @@ function Management({ info, fetchInfo }) {
     }
     const username = data.username;
     delete data.username;
-    
+
     const update = () => {
       fetch(`https://api.contentful.com//spaces/${spaceId}/environments/master/entries/UIushXQv9bsjZ5hAWxmUz`, {
         method: 'GET',
@@ -133,79 +258,32 @@ function Management({ info, fetchInfo }) {
     update();
   }
   const handleSelect = (selected) => {
+    setProfile();
+    setSelected(selected.value);
+    if (info[selected.value].profilePic) {
+      setProfile(info[selected.value].profilePic.url);
+    }
     const initialVal = Object.assign({}, { username: selected.value }, info[selected.value]);
     reset(initialVal);
   }
-  const readFile = (e) => {
-    const reader = new FileReader();
-    reader.readAsBinaryString(e.target.files[0]);
-    reader.onload = function() {
-      fetch(`https://upload.contentful.com/spaces/${spaceId}/uploads`, {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/octet-stream",
-          Authorization: `Bearer ${cmaToken}`
-        },
-        body: reader.result, 
-      })
-      .then(res => {
-        console.log(res.status);
-        return res.json();        
-      })
-      .then(res => console.log(res));        
-    }
-  }
-  //4puf8v7NmS98vea3ZN5dGj
-  //4RqaR2WOfwtzJ3NweDgaG4
-
-  const uploadPic = () => {
-    const resources = {
-      "fields": {
-          "title": {
-              "en-US": "uploaded"
-          },
-          "file": {
-              "en-US": {
-                  "contentType": "image/jpeg",
-                  "fileName": "profile.jpg",
-                  "uploadFrom": {
-                      "sys": {
-                        "type": "Link",
-                        "linkType": "Upload",
-                        "id": "4puf8v7NmS98vea3ZN5dGj"
-                      }
-                  }
-              }
-          }
-      }
-    }
-    fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/octet-stream",
-        Authorization: `Bearer ${cmaToken}`
-      },
-      body: JSON.stringify(resources),
-    }).then(res => res.json()).then(res => console.log(res));
-  }
-
-  const process = () => {
-    fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/4RqaR2WOfwtzJ3NweDgaG4/files/en-US/process`, {
-      method: 'PUT',
+  const testFunc = function() {
+    fetch(`https://api.contentful.com/spaces/${spaceId}/environments/master/assets/2XM90i0ghRTEiQIXHl7V6G`, {
+      method: 'GET',
       headers: {
         Authorization: `Bearer ${cmaToken}`,
-        "X-Contentful-Version": 1
       }
-    }).then(res => console.log(res.status));
+    }).then(res => res.json()).then(res => console.log(res));
   }
 
   return (
     <main>
       <label>Select tutor </label>
       <Select className="select" options={options} onChange={handleSelect} />
+      <div className='profile-container'>{profile ? <img className="profilePic" src={profile} /> : <div style={{ marginTop: "35px" }}>profile picture</div>}</div>
+      <ChangeProfile />
       <form onSubmit={handleSubmit(onSubmit)}>
         {infoKeys.map(e => {
-          if (e != 'courses') {
+          if (!skip.includes(e)) {
             return (
               <p key={e}>
                 <label>{e}: </label>
@@ -219,11 +297,9 @@ function Management({ info, fetchInfo }) {
           <label>courses: </label>
           {courseOptions.map(e => <span key={e}><input type='checkbox' label={e} value={e} {...register('courses')} /><label className='small-label'>{e} </label></span>)}
         </p>
-        <input type='file' onChange={readFile} />
         <button type='submit'>Update</button>
       </form>
-      <button onClick={uploadPic}>Upload</button>
-      <button onClick={process}>Process</button >
+      <button onClick={testFunc}>Test</button>
     </main>
   )
 }
@@ -252,7 +328,7 @@ export default function App() {
       .then(res => res.json())
       .then(res => {
         const tutorInfo = res.data.tutorsCollection.items[0].tutorInfo;
-        const shift = Array.from(Array(4), () => Array.from(Array(2), ()=>[]));
+        const shift = Array.from(Array(4), () => Array.from(Array(2), () => []));
         const courseMap = new Map();
         for (let student in tutorInfo) {
           const currInfo = tutorInfo[student];
@@ -261,7 +337,7 @@ export default function App() {
             const day = currInfo.day[i];
             const index = days.indexOf(day);
             shift[index][0].push(`${currInfo.name} (${currInfo.subject})` + '\n' + currInfo.time[i]);
-            currInfo.profilePic? shift[index][1].push(currInfo.profilePic.url) : shift[index][1].push(null);
+            currInfo.profilePic ? shift[index][1].push(currInfo.profilePic.url) : shift[index][1].push(null);
           }
           //Making the course-tutor correspondence            
           for (let i = 0; i < currInfo.courses.length; i++) {
@@ -274,7 +350,7 @@ export default function App() {
         }
         const courses = [];
         courseMap.forEach((val, key) => {
-          val.sort((a,b) => {
+          val.sort((a, b) => {
             a = a.split(" ")[1];
             b = b.split(" ")[1];
             if (a <= b) {
@@ -298,18 +374,18 @@ export default function App() {
       fetchInfo();
     }
   }, []);
-/*
-  useEffect(() => {
-    fetch(`https://api.contentful.com//spaces/${spaceId}/environments/master/assets/N9gEB61AGIwayN76JMl1x`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${cmaToken}`,
-      }
-    })
-    .then(res => res.json())
-    .then(res => console.log(res));
-  }, []);
-*/
+  /*
+    useEffect(() => {
+      fetch(`https://api.contentful.com//spaces/${spaceId}/environments/master/assets/N9gEB61AGIwayN76JMl1x`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${cmaToken}`,
+        }
+      })
+      .then(res => res.json())
+      .then(res => console.log(res));
+    }, []);
+  */
   if (!info) {
     return (
       <main>
