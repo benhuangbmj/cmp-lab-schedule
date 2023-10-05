e947a13e1a457bc445968dc836b2b022f7ffd8c0
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-10-05 03:39:00 +0000

    Isolated the Schedule component

--- src/App.jsx
+++ src/App.jsx
@@ -6,64 +6,11 @@ import { useForm } from 'react-hook-form';
 import { getSingleAsset } from './api-operations';
 import {sortByLastName} from './util';
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+import Schedule from './Schedule';
+
 const spaceId = import.meta.env.VITE_SPACE_ID;
 const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 const cmaToken = import.meta.env.VITE_CMA_TOKEN;
-
-function Tutors({ info }) {
-  return (
-    <td>
-      {info[0].map((e, i) =>
-        <div className="left-align" key={e}>
-          <img className='profile-pic-small' src={info[1][i]} />
-          <pre>{e}</pre>
-        </div>
-      )}
-    </td>
-  )
-}
-function Schedule({ shift, courses }) {
-  const currDate = new Date();
-  return (
-    <main>
-      <h1>CMP Tutor Schedule<br />{currDate.getMonth() >= 6 ? "Fall" : "Spring"} {currDate.getFullYear()}</h1>
-      <table>
-        <thead>
-          <tr>
-            {
-              days.map(e => {
-                return (
-                  <th key={e}>{e}</th>
-                )
-              })
-            }
-          </tr>
-          <tr>
-            {days.map((e, i) => {
-              return (
-                <Tutors key={e} info={shift[i]} />
-              )
-            })}
-          </tr>
-        </thead>
-      </table>
-      <Personnel courses={courses} />
-    </main>
-  )
-}
-
-function Personnel({ courses }) {
-  return (
-    <div>
-      <h2 style={{ 'textAlign': 'left', 'paddingLeft': '1rem' }}>Who can help you?</h2>
-      <ul>{courses.map(e => (
-        <li key={e}><pre>{e}</pre></li>
-      ))}
-      </ul>
-    </div>
-  )
-}
 
 function Management({ info, fetchInfo, setRerender, update }) {
   const [newPic, setNewPic] = useState();
