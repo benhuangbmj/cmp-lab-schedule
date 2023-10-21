c2e9344be1f774d9b06c94a13967971afa5fa919
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-10-21 04:22:57 +0000

    updated

--- src/IDcard/IDCard.jsx
+++ src/IDcard/IDCard.jsx
@@ -1,22 +1,22 @@
 import { Text, View, StyleSheet, Image, Font } from '@react-pdf/renderer';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
-import {icons} from '../util';
+import { icons } from '../util';
 
 
 const cardTitle = ' STUDENT TUTOR ';
-const [cw,ch] = [4,3];
-const [iw,ih] = ['1.2in', '1.6in'];
-const [companyBackground, display, borderSetting] = ['#002856', 'initial', "none"];
+const [cw, ch] = [4, 3];
+const [iw, ih] = ['0.96in', '1.28in'];
+const [companyBackground, display, borderSetting] = ['#002856', 'initial', "1px solid red"];
 
-Font.register({family: 'Priori', src: '/zPrioriSansOT-Regular.otf'});
-Font.register({family: 'Priori', fontWeight: 'bold', src: '/PrioriSansOT-Bold.otf'});
-Font.register({family: 'Priori', fontWeight: 'light', src: '/PrioriSansOT-Light.otf'});
-Font.register({family: 'Priori-Black', src: '/PrioriSansOT-Black.otf'});
-Font.register({family: 'Aptifer-Slab', src: '/Aptifer Slab LT Pro.otf'});
-Font.register({family: 'Aptifer-Slab-Black', src: '/Aptifer Slab LT Pro Black.otf'});
+Font.register({ family: 'Priori', src: '/zPrioriSansOT-Regular.otf' });
+Font.register({ family: 'Priori', fontWeight: 'bold', src: '/PrioriSansOT-Bold.otf' });
+Font.register({ family: 'Priori', fontWeight: 'light', src: '/PrioriSansOT-Light.otf' });
+Font.register({ family: 'Priori-Black', src: '/PrioriSansOT-Black.otf' });
+Font.register({ family: 'Aptifer-Slab', src: '/Aptifer Slab LT Pro.otf' });
+Font.register({ family: 'Aptifer-Slab-Black', src: '/Aptifer Slab LT Pro Black.otf' });
 
-const styles = StyleSheet.create({  
+const styles = StyleSheet.create({
   card: {
     border: '2pt dotted black',
     width: `${cw}in`,
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     border: borderSetting,
     objectFit: 'fill',
     width: iw,
-    height: ih,    
+    height: ih,
   },
   content: {
     border: borderSetting,
@@ -40,6 +40,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     alignContent: 'space-between',
     alignItems: 'flex-end',
+    justifyContent: 'space-between',  
   },
   company: {
     border: borderSetting,
@@ -48,19 +49,19 @@ const styles = StyleSheet.create({
     backgroundColor: companyBackground,
   },
   department: {
-    width: '60%',
+    width: '2.6in',
     height: "50%",
     border: borderSetting,
   },
-  profile: {    
+  profile: {
     border: borderSetting,
     width: iw,
-    height: ih,     
+    height: ih,
   },
   information: {
     boxSizing: "border-box",
     border: borderSetting,
-    width: '60%',
+    width: '2.6in',
     height: "50%",
     fontFamily: 'Priori',
     fontWeight: "light",
@@ -77,9 +78,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#1C8AC2",
   },
   rotate: {
-    width: `${1/(cardTitle.length)*ch*0.97}in`,
-    height: `${1/(cardTitle.length)*ch*0.97}in`,
-    fontSize: `${1/(cardTitle.length)*ch*0.8}in`,
+    width: `${1 / (cardTitle.length) * ch * 0.97}in`,
+    height: `${1 / (cardTitle.length) * ch * 0.97}in`,
+    fontSize: `${1 / (cardTitle.length) * ch * 0.8}in`,
     textAlign: "center",
     marginLeft: "20%",
     transform: 'rotate(90deg)',
@@ -89,12 +90,17 @@ const styles = StyleSheet.create({
   },
   logo: {
     objectFit: 'cover',
-    width: '100%',
+    height: '35%',
+    display: display,
+    border: borderSetting,
+  },
+  brand: {
+    objectFit: 'cover',
     display: display,
     border: borderSetting,
   },
   decoration: {
-    width: '40%',
+    width: '0.96in',
     border: borderSetting,
     height: '31.5%',
     paddingTop: '2px',
@@ -104,91 +110,101 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: '4pt',    
+    marginTop: '4pt',
     justifyContent: "flex-start",
-    textAlign: 'left',        
+    textAlign: 'left',
   },
   icon: {
     margin: 'auto',
     width: '50px',
     height: '50px',
+  },
+  social: {
+    border: borderSetting,
+    width: '100%',
+    height: '65%',
   }
 })
 
-export default function IDCard ({user}) {
-  /*const [qr, setQr] = useState({});
+export default function IDCard({ user }) {
+  const [qr, setQr] = useState({});
 
   useEffect(() => {    
     if(user.links) {
       Object.keys(user.links).forEach(link => {
-        if(user.links[link]) {
+        if(user.links[link] != null) {
+          console.log(link, user.links[link]);
           QRCode.toDataURL(user.links[link])
           .then(res => {
             const output = {};
             output[link] = res;
-            setQr(prev => Object.assign(prev, output));
+            setQr(prev => {
+              console.log(output);
+              return Object.assign(prev, output)});
           })
         }
       })
     }
-  }, []);*/
-  
-  return (    
-    <View style={styles.card} wrap={false}>      
+  }, []);
+  return (
+    <View style={styles.card} wrap={false}>
       <View style={styles.company}>
-        <Image style={[styles.logo, {backgroundColor: companyBackground}]} src='src/img/MULogo-DeptCMP-White-Horiz.png'/>
+        <Image style={[styles.brand, { backgroundColor: companyBackground }]} src='src/img/MULogo-DeptCMP-White-Horiz.png' />
       </View>
       <View style={styles.content}>
         <View style={styles.department}>
+          <View style={styles.social}>
+            {qr.linkedin && <Image style={styles.icon} src = {qr.linkedin}/>}
+          </View>
           <Image style={styles.logo} src='src/img/see anew-purpleblueword.jpg' />
         </View>
         <View style={styles.information}>
-          <Text style={{fontSize: '15pt', fontFamily:"Priori-Black", fontWeight:"normal",}}>{user.name + ' (' + user.subject + ')'}</Text>
-          <Text >{user.day.map((e,i) => `${e} ${user.time[i]}`)}</Text>
+          <Text style={{ fontSize: '15pt', fontFamily: "Priori-Black", fontWeight: "normal", }}>{user.name + ' (' + user.subject + ')'}</Text>
+          <Text >{user.day.map((e, i) => `${e} ${user.time[i]}`)}</Text>
           <View style={styles.subjects}>
-            {user.courses && user.courses.map(e => <Text key = {e} style={{width:'33%'}}>{e}</Text>)}
+            {user.courses && user.courses.map(e => <Text key={e} style={{ width: '33%' }}>{e}</Text>)}
           </View>
-          
-        </View>        
+
+        </View>
         <View style={styles.profile}>
           {
-            (user.profilePic && user.profilePic.transform)?
-            <Image 
-              style={
-                [
-                  styles.image, 
-                  {
-                    transform: user.profilePic.transform, 
-                    width: ih, 
-                    height: iw, 
-                    marginTop: '0.2in', 
-                    marginLeft: "-0.2in" 
-                  }
-                ]
-              }
-              src= {
-                user.profilePic? 
-                'https:' + user.profilePic.url:
-                'https://images.ctfassets.net/o0mxfnwxsmg0/7wk9sXm2sQjMhg7pmyffqr/39c218f89506084b406222c6ee680905/question-mark-hacker-attack-mask-preview.jpg'
-              }
-            />:
-            <Image 
-              style={styles.image}
-              src= {
-                user.profilePic? 
-                'https:' + user.profilePic.url:
-                'https://images.ctfassets.net/o0mxfnwxsmg0/7wk9sXm2sQjMhg7pmyffqr/39c218f89506084b406222c6ee680905/question-mark-hacker-attack-mask-preview.jpg'
-              }
-            />
+            (user.profilePic && user.profilePic.transform) ?
+              <Image
+                style={
+                  [
+                    styles.image,
+                    {
+                      transform: user.profilePic.transform,
+                      width: ih,
+                      height: iw,
+                      marginTop: '0.16in',
+                      marginLeft: "-0.16in"
+                    }
+                  ]
+                }
+                src={
+                  user.profilePic ?
+                    'https:' + user.profilePic.url :
+                    'https://images.ctfassets.net/o0mxfnwxsmg0/4dgxIfA7J2Mf8r3oOj9NF5/f302b65565292fef34ebff268a96edfe/question-mark-hacker-attack-mask-preview.jpg'
+                }
+              /> :
+              <Image
+                style={styles.image}
+                src={
+                  user.profilePic ?
+                    'https:' + user.profilePic.url :
+                    'https://images.ctfassets.net/o0mxfnwxsmg0/4dgxIfA7J2Mf8r3oOj9NF5/f302b65565292fef34ebff268a96edfe/question-mark-hacker-attack-mask-preview.jpg'
+                }
+              />
           }
-          
+
         </View>
         <View style={styles.decoration}>
-          <Image style={styles.icon} src='src/img/tutorForm.png'/>
+          <Image style={styles.icon} src='src/img/tutorForm.png' />
         </View>
-      </View>      
-      <View style={styles.category}>{cardTitle.split("").map((e)=><Text key={e} style={styles.rotate}>{e}</Text> )}</View>
-    </View> 
+      </View>
+      <View style={styles.category}>{cardTitle.split("").map((e) => <Text key={e} style={styles.rotate}>{e}</Text>)}</View>
+    </View>
   )
-  //<Image style={styles.icon} src = {qr[link]}/>
+  //
 }
