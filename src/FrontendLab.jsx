e2c70fa03412cb9ba7b599a844d2686b72113232
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-10-24 03:54:46 +0000

    Created the "Show ID cards" button.

--- src/FrontendLab.jsx
+++ src/FrontendLab.jsx
@@ -3,16 +3,25 @@ import {useRef, useEffect, useState} from 'react';
 import Scheduling from './scheduling/Scheduling'
 import CardDisplay from './IDcard/CardDisplay';
 
-import {icons} from './util';
   
 export default function FrontendLab({info, fetchInfo}) {
+  const [display, setDisplay] = useState('none');
+
   
   return(
     <>      
-      <Scheduling info={info} fetchInfo={fetchInfo}  />      
-      <div style={{width: '100%', height: '100vh'}}>
-{        //<CardDisplay pageSize='LETTER' pageOrientation='portrait' info={info} toolbar={true}/>
-}      </div>      
+      <Scheduling info={info} fetchInfo={fetchInfo}  />
+      
+      <button 
+        type='button' 
+        onClick={() => {
+          setDisplay('block');        
+      }}>
+        Show ID cards
+      </button>
+      <div style={{width: '100%', height: '100vh', display: display}}>
+        <CardDisplay pageSize='LETTER' pageOrientation='portrait' info={info} toolbar={true}/>
+      </div>      
     </>
   )
 }
