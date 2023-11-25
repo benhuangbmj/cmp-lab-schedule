46105c3aa58b545b14e065ee7944eff3966491fe
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-11-25 02:13:50 +0000

    updated ResetPassword

--- src/management/ResetPassword.jsx
+++ src/management/ResetPassword.jsx
@@ -1,20 +1,25 @@
-export default function ResetPassword() {
-  const generateVerificationCode = () => {
-    const codeArr = Array(4).fill();
-    codeArr.forEach((e,i) => {
-      codeArr[i] = Math.floor(Math.random() * 10);    
-    });
-    const code = codeArr.join('');
-    const output = {
-      code: code,
-      generatedTime: new Date().getTime()
-    };
-    return output;
-  }
+import {update} from '/src/api-operations';
 
-  console.log(generateVerificationCode());
-  
+export default function ResetPassword({disabled, user, fetchInfo}) {
   return (
-    <button type='button'>Reset Password</button>
+    <button type='button' disabled={disabled} onClick={() => handleClick(user, fetchInfo)}>Reset Password</button>
   )
+}
+
+const generateVerificationCode = () => {
+  const codeArr = Array(4).fill();
+  codeArr.forEach((e,i) => {
+    codeArr[i] = Math.floor(Math.random() * 10);    
+  });
+  const code = codeArr.join('');
+  const output = {
+    code: code,
+    generatedTime: new Date().getTime()
+  };
+  return output;
+}
+
+const handleClick = (user, fetchInfo) => {
+  const verificationCode = generateVerificationCode();
+  update('resetPassword', [user], verificationCode, fetchInfo);
 }
