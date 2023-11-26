4fe88f5f71fc77d619589d7da0e3ccaab1e3e765
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-11-26 23:33:02 +0000

    tested emailjs library

--- src/management/ResetPassword.jsx
+++ src/management/ResetPassword.jsx
@@ -4,15 +4,31 @@ import {update} from '/src/api-operations';
 import sendEmail from './sendEmail';
 import Popup from 'reactjs-popup';
 import bcrypt from 'bcryptjs';
+import emailjs from '@emailjs/browser';
 import 'reactjs-popup/dist/index.css';
 
+//fix: rename the variables
+const templateId = import.meta.env.VITE_EMAIL_TEMPLATE_ID;
+const emailjsPublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+const serviceId = import.meta.env.VITE_EMAIL_SERVICE_ID;
+console.log(serviceId, templateId, emailjsPublicKey);//delete
+
 export default function ResetPassword({disabled, user, fetchInfo, info}) {
-  const [open,setOpen] = useState(true);
+  const [open,setOpen] = useState(false);
   const codeEntered = useRef();
   return (
     <>
       <button type='button' disabled={disabled} onClick={() => handleClick(user, fetchInfo)}>Reset Password</button>
       <button type='button' onClick={()=>setOpen(true)}>Open</button>{//delete
+      }
+      <button type='button' onClick={() => {
+        emailjs.send(serviceId, templateId, {message: 'testing'}, emailjsPublicKey)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+      } }>Send email</button> {//delete
       }
       <Popup open={open} closeOnDocumentClick={false}>
       <div className='modal'>
