8717741b84f4077b83cf9e3f6627bee476a33745
Ben Huang <benjaminwong1985@gmail.com>
2024-01-27 00:00:04 -0500

    feat: categorize Personnel by subjects

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -36,8 +36,14 @@ export default function Schedule({ shift, courses }) {
   })
   return (
     <main>
-      <div ref={toPrint} style={{ textAlign: 'center' }}>
-        <h1>CMP Tutor Schedule<br />{currDate.getMonth() >= 6 ? "Fall" : "Spring"} {currDate.getFullYear()}</h1>
+      <div ref={toPrint} className='letter-size  flexbox-column' style={{textAlign: 'center'}}>
+        <h1 style={{margin: "0"}}>
+        <img className='qr-code' src='src/img/static-qr-code-6939aa416818b250434bfed8a036658a.png' />
+        <div style={{display: 'inline-block', position:'relative', top: '-30px'}}>
+          CMP Tutor Schedule<br/>{currDate.getMonth() >= 6 ? "Fall" : "Spring"} {currDate.getFullYear()}
+        </div>        
+        <img className='qr-code' src='src/img/qr-code.png' />
+        </h1>
         <table>
           <thead>
             <tr>
@@ -61,9 +67,6 @@ export default function Schedule({ shift, courses }) {
           </tbody>
         </table>
         <Personnel courses={courses} />
-        <p>Follow us on social media</p>
-        <img className='qr-code' src='src/img/static-qr-code-6939aa416818b250434bfed8a036658a.png' />
-        <img className='qr-code' src='src/img/qr-code.png' />
       </div>
       <button type='button' onClick={handlePrint}>Print the schedule</button>
     </main>
@@ -102,7 +105,7 @@ function Tutors({ info }) {
         {info.map((e, i) =>
           <div className="left-align" key={e[0]}>
             <div className='profile-pic-small'>
-              <img className="profilePic" src={e[1] ? e[1] : 'https://www.messiah.edu/images/4_see_your_possibilities_anew.jpg'} />
+              <img className="profilePic" src={e[1] ? e[1] : 'https://www.messiah.edu/images/4_see_your_possibilities_anew.jpg'} style={!e[1]? {objectFit: 'contain'}:{}}/>            
             </div>
             <pre className={getSubject(e[0])}>{e[0]}</pre>
           </div>
@@ -121,17 +124,39 @@ function Tutors({ info }) {
 
 
 function Personnel({ courses }) {
+  const subjects = ['CIS', 'Math', 'Physics', 'Statistics'];
+  const index = {
+    CIS: 0,
+    MATH: 1,
+    PHYS: 2,
+    STAT: 3
+  }
+  const categories = [[],[],[],[]];
+  courses.forEach((e) => {
+    const subject = e.match(/^\D+/)[0];
+    categories[index[subject]].push(e);
+  })
   return (
     <div>
-      <h2 style={{ 'textAlign': 'left', 'paddingLeft': '1rem' }}>Who can help you?</h2>
-      <div className="course-grid">
-        {courses.map(e => {
-          const arr = e.split(':');
-          return (
-            <pre key={e} style={{ width: '100%', textAlign: 'left', height: "100%" }}><strong>{arr[0]}</strong>:{arr[1]} </pre>
-          )
-        })}
-      </div>
+      <h2 style={{ margin: '.5rem 0 -0.5rem 0' }}>Who can help you?</h2>
+      {categories.map((subject,i) => {
+        return (
+          <div>
+            <div className='flexbox-row' style={{textAlign: 'left'}}>
+              <h3 className={subjects[i].toLowerCase()  + ' subject-title'}>{subjects[i]}</h3>
+              <div className='divider-line'></div> 
+            </div>           
+            <div className="course-grid" key={subjects[i]}>            
+              {subject.map(e => {
+                const arr = e.split(':');
+                return (
+                  <pre key={e} className={subjects[i].toLowerCase()} style={{ width: '100%', textAlign: 'left', height: '100%', fontSize: '8pt' }}><strong>{arr[0]}</strong>:{arr[1]} </pre>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
