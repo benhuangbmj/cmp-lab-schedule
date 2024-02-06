f4a955676cbecadc19d2b8246ff0ed8eaf98eee4
Ben Huang <benjaminwong1985@gmail.com>
2024-02-06 01:43:36 -0500

    feat: build features for Charts

--- src/charts/Charts.jsx
+++ src/charts/Charts.jsx
@@ -1,18 +1,60 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
+import readXlsxFile from "read-excel-file";
+
 import Button from "react-bootstrap/Button";
+
 export default function Charts() {
 	const fileInputRef = useRef();
+	const [endDate, setEndDate] = useState();
 
-	function handleUpload() {
-		fileInputRef.current?.click();
-		console.log(fileInputRef.current);
+	function handleUpload(e) {
+		readXlsxFile(e.target.files[0]).then((rows) => {
+			console.log(rows);
+		});
 	}
+
+	function handleSelectDate(e) {
+		const selectedDate = new Date(e.target.value);
+		window[e.target.dataset.set_state](selectedDate);
+	}
+
+	useEffect(() => {
+		console.log(endDate);
+	}, [endDate]);
+
+	useEffect(() => {
+		window.setEndDate = setEndDate;
+	}, []);
+
 	return (
 		<div>
-			<Button className="center-fit" onClick={handleUpload}>
-				Up load a data file
+			<Button
+				className="center-fit"
+				onClick={() => fileInputRef.current?.click()}
+			>
+				Upload the data file
 			</Button>
-			<input type="file" ref={fileInputRef} className="non-display" />
+			<input
+				type="file"
+				ref={fileInputRef}
+				className="non-display"
+				onChange={handleUpload}
+			/>
+			<span>Start date</span>
+			<Button
+				as="input"
+				type="date"
+				variant="outline-dark"
+				onChange={handleSelectDate}
+			/>
+			<span>End date</span>
+			<Button
+				as="input"
+				type="date"
+				variant="outline-dark"
+				data-set_state={"setEndDate"}
+				onChange={handleSelectDate}
+			/>
 		</div>
 	);
 }
