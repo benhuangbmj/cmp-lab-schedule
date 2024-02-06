321db24368e069f8316aded3a656bc7de4629e80
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2024-02-06 15:18:36 +0000

    build: update configuration files

--- replit.nix
+++ replit.nix
@@ -1,8 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-19_x
+    pkgs.nodejs_20
 		pkgs.nodePackages.typescript-language-server
 		pkgs.yarn
-		pkgs.replitPackages.jest
+		#pkgs.replitPackages.jest
 	];
 }
