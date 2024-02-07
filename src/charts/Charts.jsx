2eecd22d836fcb54e9be89c238c18e2d43f177f8
Ben Huang <benjaminwong1985@gmail.com>
2024-02-06 23:50:39 -0500

    feat: set initial value for start and end date

--- src/charts/Charts.jsx
+++ src/charts/Charts.jsx
@@ -2,6 +2,7 @@ import { useRef, useEffect, useState } from "react";
 import { useSetStates } from "/src/hooks/customHooks.jsx";
 import Papa from "papaparse";
 import _ from "lodash";
+import dayjs from "dayjs";
 
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import Button from "react-bootstrap/Button";
@@ -22,9 +23,18 @@ const headers = [
 export default function Charts() {
   const fileInputRef = useRef();
   const refSetDates = useRef({});
-  const startDate = useSetStates("startDate", refSetDates);
-  const endDate = useSetStates("endDate", refSetDates);
-  const [dataPoints, setDataPoints] = useState();
+  const startDate = useSetStates(
+    "startDate",
+    refSetDates,
+    dayjs().year() - 1 + "-01-01",
+  );
+  const endDate = useSetStates(
+    "endDate",
+    refSetDates,
+    dayjs().format("YYYY-MM-DD"),
+  );
+  const [fullData, setFullData] = useState();
+  const [plot, setPlot] = useState();
 
   function handleUpload(e) {
     Papa.parse(e.target.files[0], {
@@ -34,33 +44,39 @@ export default function Charts() {
       },
       skipEmptyLines: true,
       complete: function (results) {
-        const counts = new Map();
-        results.data.forEach((user) => {
-          const date = new Date(user.startTime);
-          const dateStr = date.toDateString();
-          const count = counts.get(dateStr);
-          if (count) {
-            counts.set(dateStr, count + 1);
-          } else {
-            counts.set(dateStr, 1);
-          }
-        });
-        const countsArr = [];
-        counts.forEach((val, key) => {
-          countsArr.push({
-            date: key,
-            count: val,
-          });
-        });
-        setDataPoints(countsArr);
+        setFullData(results);
       },
     });
   }
 
   function handleSelectDate(e) {
-    const selectedDate = new Date(e.target.value);
-    refSetDates.current[e.target.dataset.state](selectedDate);
+    refSetDates.current[e.target.dataset.state](e.target.value);
   }
+
+  useEffect(() => {
+    const results = fullData;
+    const counts = new Map();
+    results?.data?.forEach((user) => {
+      const date = new Date(user.startTime);
+      if (date >= new Date(startDate) && date <= new Date(endDate)) {
+        const dateStr = date.toDateString();
+        const count = counts.get(dateStr);
+        if (count) {
+          counts.set(dateStr, count + 1);
+        } else {
+          counts.set(dateStr, 1);
+        }
+      }
+    });
+    const countsArr = [];
+    counts.forEach((val, key) => {
+      countsArr.push({
+        date: key,
+        count: val,
+      });
+    });
+    setPlot(countsArr);
+  }, [fullData, startDate, endDate]);
 
   return (
     <div>
@@ -86,11 +102,12 @@ export default function Charts() {
             type="date"
             variant="outline-dark"
             data-state={key}
+            value={eval(key)}
             onChange={handleSelectDate}
           />
         </span>
       ))}
-      <LineChart width={1500} height={500} data={dataPoints}>
+      <LineChart width={500} height={500} data={plot}>
         <Line type="monotone" dataKey="count" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="date" />
