7c7d294bc00d386fb1f0700108e5ab866805a43d
Ben Huang <benjaminwong1985@gmail.com>
2024-02-06 21:53:29 -0500

    feat: create the framework for LineChart

--- src/charts/Charts.jsx
+++ src/charts/Charts.jsx
@@ -3,11 +3,12 @@ import { useSetStates } from "/src/hooks/customHooks.jsx";
 import Papa from "papaparse";
 import _ from "lodash";
 
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import Button from "react-bootstrap/Button";
 
 const headers = [
   "id",
-  "starTime",
+  "startTime",
   "completionTime",
   "email",
   "name",
@@ -23,16 +24,35 @@ export default function Charts() {
   const refSetDates = useRef({});
   const startDate = useSetStates("startDate", refSetDates);
   const endDate = useSetStates("endDate", refSetDates);
+  const [dataPoints, setDataPoints] = useState();
 
   function handleUpload(e) {
     Papa.parse(e.target.files[0], {
       header: true,
-      skipEmptyLines: true,
-      complete: function (results) {
-        console.log(results);
-      },
       transformHeader: function (header, i) {
         return headers[i];
+      },
+      skipEmptyLines: true,
+      complete: function (results) {
+        const counts = new Map();
+        results.data.forEach((user) => {
+          const date = new Date(user.startTime);
+          const dateStr = date.toDateString();
+          const count = counts.get(dateStr);
+          if (count) {
+            counts.set(dateStr, count + 1);
+          } else {
+            counts.set(dateStr, 1);
+          }
+        });
+        const countsArr = [];
+        counts.forEach((val, key) => {
+          countsArr.push({
+            date: key,
+            count: val,
+          });
+        });
+        setDataPoints(countsArr);
       },
     });
   }
@@ -70,6 +90,12 @@ export default function Charts() {
           />
         </span>
       ))}
+      <LineChart width={1500} height={500} data={dataPoints}>
+        <Line type="monotone" dataKey="count" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="date" />
+        <YAxis />
+      </LineChart>
     </div>
   );
 }
