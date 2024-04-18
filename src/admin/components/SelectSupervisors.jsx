168e7067421bee0d0064b18e384636a65b8ab2a7
Ben Huang <benjaminwong1985@gmail.com>
2024-04-18 16:35:41 -0400

    increment: send the supervisors data to the backend

--- src/admin/components/SelectSupervisors.jsx
+++ src/admin/components/SelectSupervisors.jsx
@@ -2,10 +2,19 @@ import Select from "react-select";
 import { useSelector } from "react-redux";
 import { selectUserData } from "/src/reducers/userDataReducer";
 import { useState, useEffect } from "react";
+import { apiBaseUrl } from "/src/utils";
 
 export default function selectSupervisors({ user, currSupOptions }) {
 	const userData = useSelector(selectUserData);
 	const [supOptions, setSupOptions] = useState([]);
+	function handleChange(values) {
+		const data = { user: user, supervisors: values };
+		fetch(apiBaseUrl + "/set-supervisors", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(data),
+		});
+	}
 	useEffect(() => {
 		if (userData && userData.status == "succeeded") {
 			const items = userData.items;
@@ -21,7 +30,7 @@ export default function selectSupervisors({ user, currSupOptions }) {
 			defaultValue={currSupOptions}
 			options={supOptions}
 			isMulti
-			onChange={(values) => console.log(values)}
+			onChange={handleChange}
 		/>
 	);
 }
