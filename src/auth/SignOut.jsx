0b73a1b02a18900e2db34ff01cb5cb96bef8b1e2
Ben Huang <benjaminwong1985@gmail.com>
2024-04-22 22:42:07 -0400

    increment: export handleSignout

--- src/auth/SignOut.jsx
+++ src/auth/SignOut.jsx
@@ -5,21 +5,27 @@ import { apiBaseUrl } from "/src/utils";
 
 import Button from "react-bootstrap/Button";
 
+export function handleSignOut(dispatch) {
+  fetch(apiBaseUrl + "/logout", {
+    method: "POST",
+    credentials: "include",
+  }).then(() => {
+    dispatch(updateActive(null));
+    const past = new Date(0);
+    document.cookie = `activeUser=; expires=${past.toUTCString()}`;
+    document.cookie = `activeStatus=; expires=${past.toUTCString()}`;
+  });
+}
 export default function SignOut() {
   const dispatch = useDispatch();
-  const handleSignOut = () => {
-    fetch(apiBaseUrl + "/logout", {
-      method: "POST",
-      credentials: "include",
-    }).then(() => {
-      dispatch(updateActive(null));
-      const past = new Date(0);
-      document.cookie = `activeUser=; expires=${past.toUTCString()}`;
-      document.cookie = `activeStatus=; expires=${past.toUTCString()}`;
-    });
-  };
   return (
-    <Button variant="outline-light" onClick={handleSignOut} size="sm">
+    <Button
+      variant="outline-light"
+      onClick={() => {
+        handleSignOut(dispatch);
+      }}
+      size="sm"
+    >
       Sign out
     </Button>
   );
