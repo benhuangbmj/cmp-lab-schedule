739078f3e25f54050ebb6a2c55b13548093a7b96
Ben Huang <benjaminwong1985@gmail.com>
2024-04-22 22:23:25 -0400

    change: disable the input if the authorization is not met

--- src/util-components/InputCheckbox.jsx
+++ src/util-components/InputCheckbox.jsx
@@ -1,37 +1,67 @@
-import { useEffect } from 'react';
-import { ErrorMessage } from '@hookform/error-message';
+import { useEffect } from "react";
+import { ErrorMessage } from "@hookform/error-message";
+import { useSelector } from "react-redux";
 
-export default function InputCheckbox({ name, utils, options, values, isReset }) {
+export default function InputCheckbox({
+  name,
+  utils,
+  options,
+  values,
+  isReset,
+}) {
   const {
     register,
     getValues,
     setValue,
     formState: { errors },
   } = utils;
-
+  const userData = useSelector((state) => state.userData.items);
+  const activeUser = useSelector((state) => state.active.user);
   useEffect(() => {
     const currValues = getValues(name);
     if (currValues && !Array.isArray(currValues)) {
-      const valueArr = Object.keys(currValues).filter(key => currValues[key]);
+      const valueArr = Object.keys(currValues).filter((key) => currValues[key]);
       setValue(name, valueArr);
     }
-  }, [isReset])
+  }, [isReset]);
 
   return (
-    <div style={{textAlign: 'left', width:"auto"}} >
-      {values.map((val) =>
-        <span key={val} className='checkbox-group'>
-          <input type='checkbox' id={`${name} ${val}`} value={val} {...register(name, options)} />
+    <div style={{ textAlign: "left", width: "auto" }}>
+      {values.map((val) => (
+        <span key={val} className="checkbox-group">
+          <input
+            disabled={!userData[activeUser].roles.developer}
+            type="checkbox"
+            id={`${name} ${val}`}
+            value={val}
+            {...register(name, options)}
+          />
           <label htmlFor={`${name} ${val}`}>{val}</label>
           <ErrorMessage
             errors={errors}
             name={name}
             render={({ messages }) =>
-              messages && <ul>
-                {Object.entries(messages).map(([type, message]) => <li style={{ width: 'fit-content', fontSize: '8pt', border: "1px solid", color: 'red' }} key={type}>{message}</li>)}
-              </ul>
-            } />
+              messages && (
+                <ul>
+                  {Object.entries(messages).map(([type, message]) => (
+                    <li
+                      style={{
+                        width: "fit-content",
+                        fontSize: "8pt",
+                        border: "1px solid",
+                        color: "red",
+                      }}
+                      key={type}
+                    >
+                      {message}
+                    </li>
+                  ))}
+                </ul>
+              )
+            }
+          />
         </span>
-      )}
-    </div>)
+      ))}
+    </div>
+  );
 }
