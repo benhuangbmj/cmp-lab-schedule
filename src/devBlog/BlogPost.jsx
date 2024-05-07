4c545b06c17a2d43e9a4d3acb7254c573a9d0215
Ben Huang <benjaminwong1985@gmail.com>
2024-05-07 09:50:10 -0400

    increment: render the rich text format with Contentful libraries

--- src/devBlog/BlogPost.jsx
+++ src/devBlog/BlogPost.jsx
@@ -1,10 +1,47 @@
 import { useEffect, useState } from "react";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+
+function renderEntry(node, children) {
+	if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+		return (
+			<iframe
+				src={node.data.target.fields.embedUrl}
+				height="100%"
+				width="100%"
+				frameBorder="0"
+				scrolling="no"
+				title={node.data.target.fields.title}
+				allowFullScreen={true}
+			/>
+		);
+	}
+}
+
+function renderAsset(node, children) {
+	return (
+		<img
+			src={`https://${node.data.target.fields.file.url}`}
+			height={node.data.target.fields.file.details.image.height}
+			width={node.data.target.fields.file.details.image.width}
+			alt={node.data.target.fields.description}
+		/>
+	);
+}
+
+const renderOptions = {
+	renderNode: {
+		[BLOCKS.EMBEDDED_ENTRY]: renderEntry,
+		[BLOCKS.EMBEDDED_RESOURCE]: renderEntry,
+		[BLOCKS.EMBEDDED_ASSET]: renderAsset,
+	},
+};
+
 export default function BlogPost({ content }) {
-	console.log(content.fields.blogPost);
 	return (
 		<div style={{ border: "1px solid black", margin: "1em auto" }}>
 			<h2>{content.fields.postTitle}</h2>
-			<p>{content.fields.blogPost.content[0].content[0].value}</p>
+			{documentToReactComponents(content.fields.blogPost, renderOptions)}
 		</div>
 	);
 }
