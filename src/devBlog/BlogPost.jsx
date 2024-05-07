896159edcb609a2d585cf3eeaa8f3ce5831b41ca
Ben Huang <benjaminwong1985@gmail.com>
2024-05-06 22:45:01 -0400

    increment: add border to the blog post container

--- src/devBlog/BlogPost.jsx
+++ src/devBlog/BlogPost.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 export default function BlogPost({ content }) {
 	console.log(content.fields.blogPost);
 	return (
-		<div>
+		<div style={{ border: "1px solid black", margin: "1em auto" }}>
 			<h2>{content.fields.postTitle}</h2>
 			<p>{content.fields.blogPost.content[0].content[0].value}</p>
 		</div>
