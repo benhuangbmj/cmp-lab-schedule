ba6b811bafb660c9bb2ad5fdf0f42195cbe6bd0b
Ben Huang <benjaminwong1985@gmail.com>
2024-05-08 14:19:36 -0400

    experiment: initiate the experiment on touch events

--- src/FrontendLab.jsx
+++ src/FrontendLab.jsx
@@ -1,8 +1,15 @@
 import { useRef, useEffect, useState } from "react";
 import PostDisplay from "/src/devBlog/PostDisplay";
 export default function FrontendLab({ info, fetchInfo }) {
+  const [color, setColor] = useState("blue")
+  useEffect(() => {
+    window.addEventListener('touchstart', ()=>{
+      setColor('red');
+    })
+  })
   return (
     <>
+    <div style={{background: color, width: "1in", height:'1in'}}></div>
       <PostDisplay />
     </>
   );
