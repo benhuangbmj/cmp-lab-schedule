44cf550e2e245b420f0e3177bae7edc4fdca8747
Ben Huang <benjaminwong1985@gmail.com>
2024-05-09 19:03:19 -0400

    experiment: experiment on multiple-touch events

--- src/FrontendLab.jsx
+++ src/FrontendLab.jsx
@@ -1,15 +1,15 @@
 import { useRef, useEffect, useState } from "react";
 import PostDisplay from "/src/devBlog/PostDisplay";
 export default function FrontendLab({ info, fetchInfo }) {
-  const [color, setColor] = useState("blue")
+  const [color, setColor] = useState("blue");
   useEffect(() => {
-    window.addEventListener('touchstart', ()=>{
-      setColor('red');
-    })
-  })
+    window.addEventListener("touchstart", (ev) => {
+      if (ev.touches.length >= 2) setColor("red");
+    });
+  });
   return (
     <>
-    <div style={{background: color, width: "1in", height:'1in'}}></div>
+      <div style={{ background: color, width: "1in", height: "1in" }}></div>
       <PostDisplay />
     </>
   );
