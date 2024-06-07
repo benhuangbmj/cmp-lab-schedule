2be6010f408c5ce0d6f5728c0b9d02da3ce8abb4
Ben Huang <benjaminwong1985@gmail.com>
2024-06-07 00:28:54 -0400

    change: sort the posts by dates

--- src/devBlog/PostDisplay.jsx
+++ src/devBlog/PostDisplay.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { apiBaseUrl } from "/src/utils";
+import dayjs from "dayjs";
 
 import BlogPost from "/src/devBlog/BlogPost";
 export default function PostDisplay() {
@@ -8,6 +9,11 @@ export default function PostDisplay() {
 		fetch(apiBaseUrl + "/blog-posts")
 			.then((res) => res.json())
 			.then((res) => {
+				res.items.sort((a, b) => {
+					const date1 = dayjs(a.sys.createdAt);
+					const date2 = dayjs(b.sys.createdAt);
+					return date2.diff(date1);
+				});
 				setBlogPosts(res.items);
 			});
 	}, []);
