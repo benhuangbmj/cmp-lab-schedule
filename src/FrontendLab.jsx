66799bdf5642f4cc79579b9024a390a67323c59e
Ben Huang <benjaminwong1985@gmail.com>
2024-06-13 13:55:28 -0400

    experiment: experiment on webcam steaming

--- src/FrontendLab.jsx
+++ src/FrontendLab.jsx
@@ -4,12 +4,27 @@ export default function FrontendLab({ info, fetchInfo }) {
   const [color, setColor] = useState("blue");
   const block = useRef();
   const block2 = useRef();
+  const refVideo = useRef();
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true,
+      })
+      .then((stream) => {
+        const video = refVideo.current;
+        video.srcObject = stream;
+        video.onloadedmetadata = (e) => {
+          video.play();
+        };
+      });
+  });
   return (
     <main
       ref={block2}
       style={{ touchAction: "none" }}
       onPointerMove={(e) => {
-        console.log("outer move", e.pointerId);
+        //console.log("outer move", e.pointerId);
         block2.current.releasePointerCapture(e.pointerId);
       }}
     >
@@ -18,17 +33,18 @@ export default function FrontendLab({ info, fetchInfo }) {
         style={{ background: color, width: "1in", height: "1in" }}
         onPointerEnter={(e) => {
           setColor("red");
-          console.log("enter", e.pointerId);
+          //console.log("enter", e.pointerId);
         }}
         onPointerMove={(e) => {
-          console.log("inner move", e.pointerId);
+          //console.log("inner move", e.pointerId);
           block.current.releasePointerCapture(e.pointerId);
         }}
         onPointerLeave={(e) => {
           setColor("blue");
-          console.log("leave", e.pointerId);
+          //console.log("leave", e.pointerId);
         }}
       ></div>
+      <video ref={refVideo} controls muted autoplay></video>
     </main>
   );
 }
