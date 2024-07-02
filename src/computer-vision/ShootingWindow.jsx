ad6a77416e2d60b62be2cf2e0fbf6c283dae0fac
Ben Huang <benjaminwong1985@gmail.com>
2024-07-02 11:01:24 -0400

    fix: collapse the video to fix the scrolling issue

--- src/computer-vision/ShootingWindow.jsx
+++ src/computer-vision/ShootingWindow.jsx
@@ -24,6 +24,8 @@ export default function ShootingWindow({ videoStream }) {
 				style={{
 					position: "absolute",
 					visibility: "hidden",
+					wdith: 0,
+					height: 0,
 				}}
 			/>
 			<canvas ref={refCanvas} />
