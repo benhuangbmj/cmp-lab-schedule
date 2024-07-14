d1cebda09a30c8c178c8618c0c95b10fb17d4c41
Ben Huang <benjaminwong1985@gmail.com>
2024-07-14 00:59:55 -0400

    experiment: experiment on loading models

--- src/computer-vision/FaceRecognition.jsx
+++ src/computer-vision/FaceRecognition.jsx
@@ -1,1 +1,26 @@
-export default function FaceRecognition() {}
+import { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
+const MODEL_URL = "/models";
+export default function FaceRecognition() {
+	const refImg = useRef();
+	useEffect(() => {
+		(async () => {
+			await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+			await faceapi.loadFaceLandmarkModel(MODEL_URL);
+			await faceapi.loadFaceRecognitionModel(MODEL_URL);
+			let fullFaceDescriptions = await faceapi
+				.detectAllFaces(refImg.current)
+				.withFaceLandmarks()
+				.withFaceDescriptors();
+			console.log(fullFaceDescriptions);
+		})();
+	});
+	return (
+		<>
+			<img
+				ref={refImg}
+				src="/src/img/fad3f6dd54f446d08b7e95580def6541.jpeg"
+			/>
+		</>
+	);
+}
