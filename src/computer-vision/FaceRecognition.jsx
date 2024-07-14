db7970d22b67556ed5bd2dc45dfaad6323d6c308
Ben Huang <benjaminwong1985@gmail.com>
2024-07-14 01:22:38 -0400

    experiment: experiment on drawing the detected face on a canvas

--- src/computer-vision/FaceRecognition.jsx
+++ src/computer-vision/FaceRecognition.jsx
@@ -1,26 +1,44 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 const MODEL_URL = "/models";
 export default function FaceRecognition() {
 	const refImg = useRef();
+	const refCanvas = useRef();
+	const [loaded, setLoaded] = useState(false);
 	useEffect(() => {
-		(async () => {
-			await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
-			await faceapi.loadFaceLandmarkModel(MODEL_URL);
-			await faceapi.loadFaceRecognitionModel(MODEL_URL);
-			let fullFaceDescriptions = await faceapi
-				.detectAllFaces(refImg.current)
-				.withFaceLandmarks()
-				.withFaceDescriptors();
-			console.log(fullFaceDescriptions);
-		})();
+		if (loaded) {
+			(async () => {
+				await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+				await faceapi.loadFaceLandmarkModel(MODEL_URL);
+				await faceapi.loadFaceRecognitionModel(MODEL_URL);
+				let fullFaceDescriptions = await faceapi
+					.detectAllFaces(refImg.current)
+					.withFaceLandmarks()
+					.withFaceDescriptors();
+				faceapi.draw.drawDetections(
+					refCanvas.current,
+					fullFaceDescriptions,
+				);
+			})();
+		}
 	});
 	return (
-		<>
+		<div>
 			<img
 				ref={refImg}
-				src="/src/img/fad3f6dd54f446d08b7e95580def6541.jpeg"
+				src="/src/img/1fc0cfd0513c4330b566d994374f607a.jpeg"
+				style={{ position: "absolute", display: "block" }}
+				onLoad={() => setLoaded(true)}
 			/>
-		</>
+			{loaded && (
+				<canvas
+					className="designing"
+					ref={refCanvas}
+					width={refImg.current?.width}
+					height={refImg.current?.height}
+					style={{ position: "absolute", display: "block" }}
+				/>
+			)}
+		</div>
 	);
 }
