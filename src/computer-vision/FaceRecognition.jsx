f36fb6e4e6edf5d39acb53042c599d12dcbfb381
Ben Huang <benjaminwong1985@gmail.com>
2024-07-14 20:00:28 -0400

    experiment: experiment on face recognition

--- src/computer-vision/FaceRecognition.jsx
+++ src/computer-vision/FaceRecognition.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 const MODEL_URL = "/models";
+import { labels } from "/src/mockData";
 export default function FaceRecognition() {
 	const refImg = useRef();
 	const refCanvas = useRef();
@@ -11,14 +12,60 @@ export default function FaceRecognition() {
 				await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
 				await faceapi.loadFaceLandmarkModel(MODEL_URL);
 				await faceapi.loadFaceRecognitionModel(MODEL_URL);
+				const labeledFaceDescriptors = await Promise.all(
+					labels.map(async (label) => {
+						// fetch image data from urls and convert blob to HTMLImage element
+						const imgUrl = `/src/img/${label}.jpg`;
+						const img = await faceapi.fetchImage(imgUrl);
+
+						// detect the face with the highest score in the image and compute it's landmarks and face descriptor
+						const fullFaceDescription = await faceapi
+							.detectSingleFace(img)
+							.withFaceLandmarks()
+							.withFaceDescriptor();
+
+						if (!fullFaceDescription) {
+							throw new Error(`no faces detected for ${label}`);
+						}
+
+						const faceDescriptors = [
+							fullFaceDescription.descriptor,
+						];
+						return new faceapi.LabeledFaceDescriptors(
+							label,
+							faceDescriptors,
+						);
+					}),
+				);
 				let fullFaceDescriptions = await faceapi
 					.detectAllFaces(refImg.current)
 					.withFaceLandmarks()
 					.withFaceDescriptors();
+				const maxDescriptorDistance = 0.6;
+				const faceMatcher = new faceapi.FaceMatcher(
+					labeledFaceDescriptors,
+					maxDescriptorDistance,
+				);
+
+				const results = fullFaceDescriptions.map((fd) =>
+					faceMatcher.findBestMatch(fd.descriptor),
+				);
 				faceapi.draw.drawDetections(
 					refCanvas.current,
 					fullFaceDescriptions,
 				);
+				faceapi.draw.drawFaceLandmarks(
+					refCanvas.current,
+					fullFaceDescriptions,
+				);
+				results.forEach((bestMatch, i) => {
+					const box = fullFaceDescriptions[i].detection.box;
+					const text = bestMatch.toString();
+					const drawBox = new faceapi.draw.DrawBox(box, {
+						label: text,
+					});
+					drawBox.draw(refCanvas.current);
+				});
 			})();
 		}
 	});
