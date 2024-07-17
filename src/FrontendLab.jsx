c156182b9b65f5dbe2f607f75a58585250faabc2
Ben Huang <benjaminwong1985@gmail.com>
2024-07-17 17:26:48 -0400

    change: provide the context to FaceRecognition

--- src/FrontendLab.jsx
+++ src/FrontendLab.jsx
@@ -1,8 +1,12 @@
 import FaceRecognition from "/src/computer-vision/FaceRecognition";
+import ImageGallery from "/src/util-components/ImageGallery";
+import { CVContextProvider } from "/src/contexts/CVContext";
 export default function FrontendLab({ info, fetchInfo }) {
   return (
     <main>
-      <FaceRecognition />
+      <CVContextProvider>
+        <FaceRecognition />
+      </CVContextProvider>
     </main>
   );
 }
