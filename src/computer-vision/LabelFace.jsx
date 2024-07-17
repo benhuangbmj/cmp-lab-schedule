436217c76188c17fb56322dd93f577796bbc548b
Ben Huang <benjaminwong1985@gmail.com>
2024-07-17 17:31:33 -0400

    change: revise the component workflow

--- src/computer-vision/LabelFace.jsx
+++ src/computer-vision/LabelFace.jsx
@@ -1,9 +1,33 @@
-import { useState, useEffect } from "react";
+import {
+	useState,
+	useEffect,
+	useRef,
+	useContext,
+	forwardRef,
+	useImperativeHandle,
+} from "react";
+import { CVContext } from "/src/contexts/CVContext";
 import * as faceapi from "face-api.js";
 
-export default function LabelFace({ labeledFaces, imgDataURL }) {
+export default forwardRef(function LabelFace({ imgDataURL }, ref) {
+	const cVContext = useContext(CVContext);
+	const {
+		dispatchLabeledFaces,
+		dispatchFaceCollection,
+		dispatchDetectedFace,
+		detectedFace,
+	} = cVContext;
 	const [labelText, setLabelText] = useState("");
-	const addLabel = async () => {
+	const [errorMessage, setErrorMessage] = useState();
+	const refDescriptor = useRef();
+	useImperativeHandle(ref, () => ({
+		reset,
+	}));
+	function reset() {
+		dispatchDetectedFace({ type: "reset" });
+		refDescriptor.current = null;
+	}
+	async function detectFace() {
 		const fullFaceDescription = await faceapi
 			.detectSingleFace(imgDataURL)
 			.withFaceLandmarks()
@@ -11,24 +35,61 @@ export default function LabelFace({ labeledFaces, imgDataURL }) {
 		if (!fullFaceDescription) {
 			throw new Error("No face detected");
 		}
-		const faceDescriptors = [fullFaceDescription.descriptor];
+		const { x, y, width, height } = fullFaceDescription.detection.box;
+		const regionsToExtract = [new faceapi.Rect(x, y, width, height)];
+		const canvases = await faceapi.extractFaces(
+			imgDataURL,
+			regionsToExtract,
+		);
+		const imgSrc = canvases[0].toDataURL();
+		dispatchDetectedFace({
+			type: "set_src",
+			payload: imgSrc,
+		});
+		refDescriptor.current = [fullFaceDescription.descriptor];
+	}
+	const addLabel = () => {
+		const faceDescriptors = refDescriptor.current;
 		const newLabeledFace = new faceapi.LabeledFaceDescriptors(
 			labelText,
 			faceDescriptors,
 		);
-		labeledFaces.push(newLabeledFace);
+		dispatchLabeledFaces({
+			type: "added",
+			payload: newLabeledFace,
+		});
+		dispatchFaceCollection({
+			type: "added",
+			payload: detectedFace.src,
+		});
+		setLabelText("");
+		reset();
 	};
+	useEffect(() => {
+		if (imgDataURL) {
+			detectFace().catch((error) => {
+				dispatchDetectedFace({
+					type: "set_error_message",
+					payload: error.message,
+				});
+			});
+		}
+	}, [imgDataURL]);
 	return (
-		<>
+		<div>
 			<input
 				value={labelText}
 				onChange={(e) => {
 					setLabelText(e.target.value);
 				}}
 			/>
-			<button type="button" onClick={addLabel}>
+			<button
+				type="button"
+				onClick={addLabel}
+				disabled={!detectedFace.src || labelText == ""}
+			>
 				Add Labeled Face
 			</button>
-		</>
+		</div>
 	);
-}
+});
