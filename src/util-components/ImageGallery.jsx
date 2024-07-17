bb5611a5dd159ad451634524e10dc11d04d32e28
Ben Huang <benjaminwong1985@gmail.com>
2024-07-17 17:29:44 -0400

    change: polish the UI layout

--- src/util-components/ImageGallery.jsx
+++ src/util-components/ImageGallery.jsx
@@ -1,20 +1,33 @@
-import { cloneElement } from "react";
-export default function ImageGallery({ collection = [] }) {
+export default function ImageGallery({ collection = [], captions = [] }) {
 	return (
 		<div
 			className="flexbox-row"
 			style={{
+				boxSizing: "content-box",
 				justifyContent: "flex-start",
 				border: "5px double black",
 				padding: "0.2em",
+				height: "7em",
+				overflowY: "auto",
+				gap: ".25em",
 			}}
 		>
-			{collection.map((img, i, arr) =>
-				cloneElement(img, {
-					key: i,
-					className: i == arr.length - 1 ? "animation-fade-in" : "",
-				}),
-			)}
+			{collection.map((src, i, arr) => (
+				<figure
+					key={i}
+					className={i == arr.length - 1 ? "animation-fade-in" : ""}
+				>
+					<img
+						src={src}
+						style={{
+							width: "5em",
+							height: "5em",
+							objectFit: "cover",
+						}}
+					/>
+					<figcaption>{captions[i]}</figcaption>
+				</figure>
+			))}
 		</div>
 	);
 }
