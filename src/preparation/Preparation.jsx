5699c5ec43b2c3b22a841e1da1d3e8c98d740bfc
Ben Huang <benjaminwong1985@gmail.com>
2024-08-15 13:15:58 -0400

    refactor: remove the outdated checkAtive function

--- src/preparation/Preparation.jsx
+++ src/preparation/Preparation.jsx
@@ -29,7 +29,6 @@ export default function () {
 			dispatch(fetchUserData());
 			fetchInfo();
 		}
-		checkActive();
 	}, [fetchInfo]);
 	useEffect(() => {
 		handleLoginCheck();
@@ -67,22 +66,6 @@ export default function () {
 					setLoginCheck(true);
 				});
 		}
-	}
-	async function checkActive() {
-		const cookieStr = document.cookie.split(";");
-		const cookie = {};
-		let activeUser = null;
-		cookieStr.forEach((str) => {
-			const [key, value] = str.split("=");
-			cookie[key.trim()] = value;
-		});
-		if (cookie.activeUser != null) {
-			const userStatus = await fetchKey(cookie.activeUser, "status");
-			if (userStatus.code == cookie.activeStatus) {
-				activeUser = cookie.activeUser;
-			}
-		}
-		dispatch(updateActive(activeUser));
 	}
 
 	return (
