550bdf81aaeae8f5694ed437990eab2126730407
Ben Huang <benjaminwong1985@gmail.com>
2024-08-20 18:05:16 -0400

    fix: apply the lazy quantifier on the regex in order to avoid overly consumption

--- deploy.cjs
+++ deploy.cjs
@@ -1,23 +1,21 @@
-require('dotenv').config();
-const fs=require('fs');
-const {exec} = require("node:child_process");
-
-let myFile = fs.readFileSync('.env.local', {encoding: 'utf8'});
-
-const regexp = /(?<=VITE_API_BASE_URL=)[\w\W]+(?=\n)/
+require("dotenv").config();
+const fs = require("fs");
+const { exec } = require("node:child_process");
+let myFile = fs.readFileSync(".env.local", { encoding: "utf8" });
+const regexp = /(?<=VITE_API_BASE_URL=)[\w\W]+?(?=\n)/;
 const original = myFile.match(regexp)[0];
 myFile = myFile.replace(regexp, process.env.API_PRODUCTION);
-
 try {
-  fs.writeFileSync('.env.local', myFile);
-} catch(err) {
-  console.log(err)
+  fs.writeFileSync(".env.local", myFile);
+} catch (err) {
+  console.log(err);
 }
 exec("npm run build", (err, output) => {
   console.log(output);
   myFile = myFile.replace(regexp, original);
-  fs.writeFileSync('.env.local', myFile);
+  fs.writeFileSync(".env.local", myFile);
   exec("bash transfer.sh", (err, output) => {
     err && console.log(err);
+    console.log(output);
   });
 });
