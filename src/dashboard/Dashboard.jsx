d6863d5c7b69f54aff421fa58856762451a89978
Ben Huang <benjaminwong1985@gmail.com>
2024-09-03 11:07:16 -0400

    feat: create TutorHours to display the work hour statistics and fix #53

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
 import Charts from "/src/dashboard/components/Charts";
+import TutorHours from "/src/dashboard/components/TutorHours";
 export default function Dashboard() {
 	return (
 		<>
 			<Charts />
+			<TutorHours />
 		</>
 	);
 }
--- src/dashboard/components/TutorHours.jsx
+++ src/dashboard/components/TutorHours.jsx
@@ -1,7 +1,36 @@
+import Table from "react-bootstrap/Table";
+import { useContext } from "react";
+import { AppContext } from "/src/contexts/AppContext";
 export default function TutorHours() {
+	const { info } = useContext(AppContext);
 	return (
 		<div>
 			<h1>Tutor Hours</h1>
+			<Table>
+				<tbody>
+					{Object.keys(info)
+						.filter((key) => info[key].inactive == false)
+						.toSorted()
+						.map((key, i, arr) => {
+							const hours = Math.max(
+								0,
+								info[key].schedule
+									?.flat()
+									.reduce(
+										(sum, e) => (e ? sum + 0.25 : sum),
+										-0.25,
+									) || 0,
+							);
+							return (
+								<tr key={`${key} ${hours} ${info[key.name]}`}>
+									<td>{key}</td>
+									<td>{info[key].name}</td>
+									<td>{hours}</td>
+								</tr>
+							);
+						})}
+				</tbody>
+			</Table>
 		</div>
 	);
 }
