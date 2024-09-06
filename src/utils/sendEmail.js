a796e5df3afd51352d34b06750095fda4313b4e9
Ben Huang <benjaminwong1985@gmail.com>
2024-09-06 16:35:09 -0400

    change: check user before sending email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,9 +1,10 @@
 import emailjs from "@emailjs/browser";
 const templateId = import.meta.env.VITE_EMAIL_TEMPLATE_ID;
 const emailjsPublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+const replyTo = import.meta.env.VITE_ERROR_RECEIVER;
 
 const sendEmail = (user, body) => {
-   if (!(templateId && emailjsPublicKey)) return;
+   if (!(templateId && emailjsPublicKey && user)) return;
    emailjs
       .send(
          "default_service",
@@ -11,7 +12,7 @@ const sendEmail = (user, body) => {
          {
             message: body,
             recipient: `${user}@messiah.edu`,
-            reply_to: "bhuang@messiah.edu",
+            reply_to: replyTo + "@messiah.edu",
          },
          emailjsPublicKey,
       )
