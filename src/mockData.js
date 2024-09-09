89cd772fbdc273081acc8845cb9acbafa549c945
Ben Huang <benjaminwong1985@gmail.com>
2024-09-08 23:35:44 -0400

    chore: upload the mock data

---
+++ src/mockData.js
@@ -0,0 +1,12 @@
+const studentData = {
+	id: "02197883",
+	name: "John Smith",
+	major: "Mathematics",
+	email: "js1234@messiah.edu",
+	subject: "Calculus III",
+	year: "Sophomore",
+};
+
+const studentId = "02197883";
+
+export { studentData, studentId };
